ac2b64ef97365bb278c001f17f44153a65f66329
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-09-23 19:33:46 +0000

    variables config done

---
+++ 01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1233
+var accountEmail = "bhyadav.2002@gmail.com"
+let accountPassword = "12345"
+accountCity = "jaipur" // allowed but not prefer to use
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
