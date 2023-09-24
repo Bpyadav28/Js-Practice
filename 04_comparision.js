a18ab41cc73c1229beb53873a718dd342b27dbc6
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-09-24 07:41:18 +0000

    comparisionConfusion config done

---
+++ 04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1) // isme aapne aap typeconversion ho jata
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) // isme diiference isliye aara hai kyuki equality or comparision operator alag alag behave krte hai
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0) // always false output
+
+// === => strict check ie it will check datatypes of variale too
+
+console.log("2" === 2) // => false
