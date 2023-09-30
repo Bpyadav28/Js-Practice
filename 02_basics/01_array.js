77943f69d75f38aa56d6fb246cfcfb45d956c5c6
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-09-30 14:35:17 +0000

    array config done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,19 @@
+const array = [1,2,3,4,5,6]
+const myArr2 = new Array(12,13,14,15)
+
+// console.log(myArr2)
+
+// console.log(array)
+// console.log(array[2])
+
+
+// ARRAY METHODS
+
+// myArr2.push(2) => inserted at last
+// myArr2.push(3)
+// myArr2.pop()  => deleted from last
+
+// myArr2.unshift(10) => inserted at beginning
+console.log(myArr2)
+
+
