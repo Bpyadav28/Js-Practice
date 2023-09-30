ca3f2177cfe1c4e401b1c56271ceaf8f8baaeae6
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-09-30 14:35:57 +0000

    deleted

--- 01_variables.js
+++
@@ -1,6 +0,0 @@
-const accountId = 1233
-var accountEmail = "bhyadav.2002@gmail.com"
-let accountPassword = "12345"
-accountCity = "jaipur" // allowed but not prefer to use
-let accountState
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
