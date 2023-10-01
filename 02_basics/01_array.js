d01d80d71431b9e96afed2d21d84804a989df979
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-10-01 06:07:37 +0000

    array part 1 done

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -14,6 +14,28 @@ const myArr2 = new Array(12,13,14,15)
 // myArr2.pop()  => deleted from last
 
 // myArr2.unshift(10) => inserted at beginning
-console.log(myArr2)
+// myArr2.shift()  => removes first element
+// console.log(myArr2)
 
+// !!!!!!  METHODS  !!!!!!!
+ 
+// console.log(myArr2.includes(9)) => gives boolean value
+// console.log(myArr2.indexOf(12))
 
+const newArr = myArr2.join()
+// console.log(newArr) => typeof string
+
+// console.log(myArr2)
+
+//  slice , splice
+
+//  slice me original array same rehta aur range last wala include ni hota
+
+//  const mna = array.slice(1,3)
+//  console.log("a", array)
+//  console.log(mna)
+
+// splice me original array change hojata or range me last wala include hota  
+ const mna1 = array.splice(1,3)
+ console.log("b", array )
+ console.log(mna1)
