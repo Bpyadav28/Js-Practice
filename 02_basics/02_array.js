c396cf15bf8975043b726c3ce5177ea53bef337c
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-10-01 06:27:35 +0000

    array part 2 done

---
+++ 02_basics/02_array.js
@@ -0,0 +1,17 @@
+const marvel_heros = ["thor", "ironman" , "spiderman"];
+const dc_heros = ["batman", "superman", "flash"];
+
+//1) push operation existing array me hi push krdeta hai elements ko aur vo element chahe toh khud ek array bhi ho sakta hai
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// 2) concat operation new array me insert krdeta hai elements ko 
+
+// const new_heros = marvel_heros.concat(dc_heros)
+// console.log(new_heros)
+
+// 3) spread operation easire way of using concat operation
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros) 
