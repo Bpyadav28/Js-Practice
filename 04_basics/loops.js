31b719c059baa541b5edc8ba98b0bee361a77cbb
Bpyadav28 <139537230+Bpyadav28@users.noreply.github.com>
2023-10-11 19:39:24 +0000

    loops added

---
+++ 04_basics/loops.js
@@ -0,0 +1,11 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach( function(item){
+    // console.log(item);
+})
+
+function printMe(val){
+    console.log(val);
+}
+
+coding.forEach(printMe)
